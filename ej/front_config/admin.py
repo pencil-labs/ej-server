81c87ee3db519a279e5401bc8835f5657127489c
pablodiegoss <pablodiegoss@hotmail.com>
2018-04-19 15:39:02 -0300

    Add Migrations for Color and configure routes

--- ej/front_config/admin.py
+++ ej/front_config/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from ej.front_config.models import SocialMedia
+from ej.front_config.models import SocialMedia, ColorPallet
 
 
 admin.site.register(SocialMedia)
+admin.site.register(ColorPallet)
--- ej/front_config/api.py
+++ ej/front_config/api.py
@@ -3,3 +3,4 @@ import ej.front_config.api_views
 
 def register(router):
     router.register(r'social_media', ej.front_config.api_views.SocialMediaViewSet)
+    router.register(r'color_pallet', ej.front_config.api_views.ColorPalletViewSet)
