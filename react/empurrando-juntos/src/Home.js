254eded32030f2501a287c196558cd76bc712d32
Caio Almeida <caiosba@gmail.com>
2018-05-13 22:10:15 -0300

    Ticket #48: React

---
+++ react/empurrando-juntos/src/Home.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Conversations from './Conversations';
+
+class Home extends Component {
+  render() {
+    return (
+      <div className="Home">
+        <Conversations />
+      </div>
+    );
+  }
+}
+
+export default Home;
